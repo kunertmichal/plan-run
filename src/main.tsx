fef1ab6df838185a57b64478ec5e01d99b8d7635
Michal Kunert <mkunert@virtuslab.com>
2025-07-16 16:14:07 +0200

    refactor: cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { App } from "./App.tsx";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
-
-console.log(convex);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
