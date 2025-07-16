c8ae438155fccb75194d3a527460c650a8cce318
Michal Kunert <mkunert@virtuslab.com>
2025-07-16 16:34:06 +0200

    chore: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export function App() {
-  return <div className="text-3xl text-red-500">Hello World</div>;
+  return (
+    <div className="text-3xl text-red-500">
+      <Button>Click me</Button>
+    </div>
+  );
 }
